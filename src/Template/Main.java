7f3940f290000cb2ba280445ff4473e388318ea4
mgmg-zbtn <takeshita6623@gmail.com>
2025-02-23 23:26:43 +0900

    update Main.java
    revert to previous state

--- src/Template/Main.java
+++ src/Template/Main.java
@@ -1,15 +1,18 @@
 package Template;
+import Test.Test;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(Test.stream("src\\N000\\N001\\testcase\\case1.txt")));
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// br.readLine();
-		
+
 		
 	}
 }
